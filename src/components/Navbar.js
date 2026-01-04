734fc0fa641fe43f792b1039196c946ebb8f01f2
Annikalla Nandhini <150242225+nandhiniannika@users.noreply.github.com>
2026-01-04 16:16:43 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,27 +3,63 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">News</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        
+        {/* Brand */}
+        <Link className="navbar-brand fw-bold fs-3" to="/">
+          News
+        </Link>
 
-        <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
-          <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
-           <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
-           <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
-      </ul>
-     
-    </div>
-  </div>
-</nav>
+        {/* Toggle button */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Menu */}
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mb-2 mb-lg-0 fw-bold fs-5">
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/business">Business</Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/entertainment">Entertainment</Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/health">Health</Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/science">Science</Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/sports">Sports</Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/technology">Technology</Link>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
