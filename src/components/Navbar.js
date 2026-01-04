664f6748cbe041a9010d7cfabbddf4a7fba2885e
Annikalla Nandhini <150242225+nandhiniannika@users.noreply.github.com>
2026-01-04 16:35:48 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,56 +4,54 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-      <div className="container-fluid">
+  <div className="container-fluid d-flex align-items-center">
 
-        {/* Brand */}
-        <Link className="navbar-brand fw-bold fs-3" to="/">
-          News
-        </Link>
+    {/* Brand */}
+    <Link className="navbar-brand fw-bold fs-3 me-4" to="/">
+      News
+    </Link>
 
-        {/* Hamburger */}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+    {/* Toggler */}
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNav"
+    >
+      <span className="navbar-toggler-icon"></span>
+    </button>
 
-        {/* Menu */}
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav fw-bold fs-5 ms-lg-3 w-100">
+    {/* Menu */}
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav fw-bold fs-5 gap-3">
 
-            <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/business">Business</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/entertainment">Entertainment</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/health">Health</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/science">Science</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/sports">Sports</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/technology">Technology</Link>
-            </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/business">Business</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/entertainment">Entertainment</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/health">Health</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/science">Science</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/sports">Sports</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/technology">Technology</Link>
+        </li>
 
-          </ul>
-        </div>
-      </div>
-    </nav>
+      </ul>
+    </div>
+  </div>
+</nav>
+
   );
 };
 
