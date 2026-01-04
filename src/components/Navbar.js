62e9f06f222011b32143759f9a1485997e0ed582
Annikalla Nandhini <150242225+nandhiniannika@users.noreply.github.com>
2026-01-04 17:04:34 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,39 +25,34 @@ const Navbar = () => {
         </button>
 
         {/* Collapsible Menu */}
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mb-2 mb-lg-0">
+       <div className="collapse navbar-collapse" id="navbarSupportedContent">
+  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
-            <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
-            </li>
+    <li className="nav-item">
+      <Link className="nav-link" to="/">Home</Link>
+    </li>
+    <li className="nav-item">
+      <Link className="nav-link" to="/business">Business</Link>
+    </li>
+    <li className="nav-item">
+      <Link className="nav-link" to="/entertainment">Entertainment</Link>
+    </li>
+    <li className="nav-item">
+      <Link className="nav-link" to="/health">Health</Link>
+    </li>
+    <li className="nav-item">
+      <Link className="nav-link" to="/science">Science</Link>
+    </li>
+    <li className="nav-item">
+      <Link className="nav-link" to="/sports">Sports</Link>
+    </li>
+    <li className="nav-item">
+      <Link className="nav-link" to="/technology">Technology</Link>
+    </li>
 
-            <li className="nav-item">
-              <Link className="nav-link" to="/business">Business</Link>
-            </li>
+  </ul>
+</div>
 
-            <li className="nav-item">
-              <Link className="nav-link" to="/entertainment">Entertainment</Link>
-            </li>
-
-            <li className="nav-item">
-              <Link className="nav-link" to="/health">Health</Link>
-            </li>
-
-            <li className="nav-item">
-              <Link className="nav-link" to="/science">Science</Link>
-            </li>
-
-            <li className="nav-item">
-              <Link className="nav-link" to="/sports">Sports</Link>
-            </li>
-
-            <li className="nav-item">
-              <Link className="nav-link" to="/technology">Technology</Link>
-            </li>
-
-          </ul>
-        </div>
 
       </div>
     </nav>
