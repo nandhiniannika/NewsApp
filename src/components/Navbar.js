577eed90985b416664a7aed45067480496a546ca
Annikalla Nandhini <150242225+nandhiniannika@users.noreply.github.com>
2026-01-04 16:09:41 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,39 +3,6 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    // <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-    //   <div className="container-fluid">
-
-    //     <Link className="navbar-brand fw-bold fs-3" to="/">
-    //       News
-    //     </Link>
-
-    //     <button
-    //       className="navbar-toggler"
-    //       type="button"
-    //       data-bs-toggle="collapse"
-    //       data-bs-target="#navbarSupportedContent"
-    //       aria-controls="navbarSupportedContent"
-    //       aria-expanded="false"
-    //       aria-label="Toggle navigation"
-    //     >
-    //       <span className="navbar-toggler-icon"></span>
-    //     </button>
-
-    //     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    //       <ul className="navbar-nav ms-auto mb-2 mb-lg-0 fw-bold fs-5">
-    //         <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-    //         <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
-    //         <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
-    //         <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
-    //         <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
-    //         <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
-    //         <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
-    //       </ul>
-    //     </div>
-
-    //   </div>
-    // </nav>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">News</a>
@@ -43,7 +10,7 @@ const Navbar = () => {
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav ms-auto mb-2 mb-lg-0 fw-bold fs-5 text-end text-lg-start">
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0 fw-bold fs-5">
 
         <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
